d3f346fd2e617163947adc5a1782e1af1353e60f
Mohamed Ndayishimiye <ndayimodi@gmail.com>
2023-11-13 11:45:55 +0100

    Add window event listener when loaded

---
+++ main.js
@@ -0,0 +1,27 @@
+window.addEventListener('load', ()=> {
+    let long;
+    let lat;
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+long = position.coords.longitude;
+lat = position.coords.latitude;
+const api = `https://api.open-meteo.com/v1/forecast?${lat}&${long}
+&hourly=temperature_2m,precipitation_probability,precipitation`
+
+        });
+
+fetch(api)
+.then(response => {
+    return response.json();
+     
+})
+.then (response => {
+    console.log(data);
+})
+
+    } else //We are doing this incase it doesn't work on user side
+    {
+        h1.textContent = "Oops, looks like you don't have location turned on"
+    }
+});
